468e96f8f412631890990ce9257bca57589d92bc
Eriri <741674999@qq.com>
2019-07-30 22:09:30 +0800

    190730

--- code/main.cpp
+++ code/main.cpp
@@ -1,103 +1,173 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i,s,t) for(int i=(s);i<(t);i++)
-#define all(x) x.begin(),x.end()
-#define sz(x) ((int)x.size())
-#define fi first
-#define se second
+#define dd(x) cout << #x << '=' << x << ' '
+#define de(x) cout << #x << '=' << x << '\n'
 
-using referential=vector<double>;
-using attribute=vector<referential>;
-using distribution=map<int,double>;
-using antecedent_weight=vector<double>;
-using rule_weight=vector<double>;
-using belief_degress=vector<double>;
+using Referential = vector<double>;
+using Distribution = map<int, double>;
 
-attribute A;referential C;
-antecedent_weight delta;
-rule_weight w;
+using Attribute = vector<Referential>;
+using Antecedent = vector<Distribution>;
+using Consequent = Distribution;
 
-void transform(referential& r,double& a,distribution& c)
+using AntecedentWeight = vector<double>;
+using RuleWeight = double;
+using ActivateWeight = double;
+using BeliefDegress = double;
+using BeliefDistribution = vector<BeliefDegress>;
+
+Attribute ATTR;
+Referential CONS;
+AntecedentWeight DELTA;
+const double EPS = 1e-5;
+
+Distribution trans_form(Referential &r, double &a)
 {
-	int y=lower_bound(all(r),a)-r.begin(),x=y-1;
-	if((y==0&&a<r[y])||y==sz(r)){}else if(a==r[y])c[y]=1.0;
-	else c[x]=(r[y]-a)/(r[y]-r[x]);c[y]=1.0-c[x];
-}
-
-struct rule
-{
-	rule(){};
-	rule(vector<double> a,double c)
+	int y = lower_bound(r.begin(), r.end(), a) - r.begin(), x = y - 1;
+	Distribution d;
+	if ((y == 0 && a < r[y]) || y == r.size())
 	{
-		antecedent.resize(A.size());
-		rep(i,0,sz(A))transform(A[i],a[i],antecedent[i]);
-		transform(C,c,consequent);
 	}
-	vector<distribution> antecedent;
-	distribution consequent;
-};
-
-double individual_match_degree(distribution& a,distribution& b)
-{
-	return accumulate(all(a),0.0,[&](double c,auto _){return c+sqrt(_.se*b[_.fi]);});
-}
-
-void calculate(rule& x,vector<rule>& r,antecedent_weight& delta,rule_weight& w)
-{
-	antecedent_weight norm_delta(delta);double max_delta=*max_element(all(delta));
-	for_each(all(norm_delta),[&](double& d){d/=max_delta;});w.assign(r.size(),1.0);
-	rep(i,0,sz(r))rep(j,0,sz(A))w[i]*=pow(
-		individual_match_degree(x.antecedent[j],r[i].antecedent[j]),norm_delta[j]);
-	double sum_w=accumulate(all(w),0.0);
-	rep(i,0,sz(w))w[i]/=sum_w;
-	for_each(all(w),[&](double w){w/=sum_w;});
-}
-
-belief_degress evidential_reasoning(vector<rule>& r,rule_weight& w)
-{
-	double N_m_D(1.0),N_m_D_(1.0),S_N_m;
-	belief_degress B(C.size(),1.0),D(r.size(),1.0);
-	rep(i,0,sz(r))rep(j,0,sz(C))D[i]-=w[i]*r[i].consequent[j];
-	rep(i,0,sz(r))N_m_D*=D[i],N_m_D_*=1.0-w[i];
-	rep(j,0,sz(C))rep(i,0,sz(r))B[j]*=r[i].consequent[j]+D[i];
-	S_N_m=accumulate(all(B),0.0);
-	rep(j,0,sz(C))B[j]=(B[j]-N_m_D)/(S_N_m-(sz(C)-1)*N_m_D-N_m_D_);
-	return B;
-}
-
-double similarity(distribution& a,distribution& b)
-{
-	double d=0.0;
-	for(auto& e:a)
+	else if (a == r[y])
+		d[y] = 1.0;
+	else
+		d[x] = (r[y] - a) / (r[y] - r[x]),
+		d[y] = 1.0 - d[x];
 	return d;
 }
 
-double sra(rule& a,rule& b)
+struct Rule
 {
-	double s=numeric_limits<double>::max();
-	for(int i=0;i<a.antecedent.size();i++)
-		s=min(s,similarity(a.antecedent[i],b.antecedent[i]));
-	return s;
+	Rule(){};
+	Rule(vector<double> ar, double cs)
+	{
+		a.resize(ATTR.size());
+		inner_product(ATTR.begin(), ATTR.end(), ar.begin(), a.begin(),
+					  [](auto ai, auto d) {*ai=d;return ++ai; }, trans_form);
+		c = trans_form(CONS, cs);
+		raw_a = ar, raw_c = cs;
+		t = 1.0, w = 1.0;
+	}
+	Antecedent a;
+	Consequent c;
+	RuleWeight t;
+	ActivateWeight w;
+	BeliefDegress d;
+	vector<double> raw_a;
+	double raw_c;
+};
+
+double match_individual(Distribution &a, Distribution b)
+{
+	double tmp = accumulate(a.begin(), a.end(), 0.0, [&](double c, auto _) { return c + sqrt(_.second * b[_.first]); });
+	return tmp;
+	/* 
+	set<int> s;
+	double d = 0.0;
+	for (auto &e : a)
+		s.insert(e.first);
+	for (auto &e : b)
+		s.insert(e.first);
+	for (auto &e : s)
+		d += (sqrt(a[e]) - sqrt(b[e])) * (sqrt(a[e]) - sqrt(b[e]));
+	return 1.0 - sqrt(d) / sqrt(2.0);
+	*/
 }
 
-double src(rule& a,rule& b)
+void normalize_delta()
 {
-	return similarity(a.consequent,b.consequent);
+	double D = *max_element(DELTA.begin(), DELTA.end());
+	for_each(DELTA.begin(), DELTA.end(), [&](double &d) { d /= D; });
 }
 
-double consistency(rule& a,rule& b)
+void activate_rule(Rule &x, vector<Rule> &r, int opt = 0, int opd = 0)
 {
-	double sa=sra(a,b),sc=src(a,b);
-	return exp(-sa*(1.0-sc));
+	for_each(r.begin(), r.end(), [&](Rule &y) {
+		y.w = opt ? y.t : 1.0;
+		inner_product(x.a.begin(), x.a.end(), y.a.begin(), DELTA.begin(),
+					  [&](auto d, double _) { y.w*=opd?pow(_,*d):_;return ++d; },
+					  [](auto &xa, auto &ya) { return match_individual(xa, ya); }); });
+	double W = accumulate(r.begin(), r.end(), 0.0, [](double c, auto &y) { return c + y.w; });
+	for_each(r.begin(), r.end(), [&](Rule &y) { y.w /= W; });
+}
+
+BeliefDistribution evidential_reasoning(vector<Rule> &r)
+{
+	BeliefDistribution beta(CONS.size(), 1.0);
+	BeliefDegress rd(1.0), ad(1.0), sd(0.0);
+	for_each(r.begin(), r.end(),
+			 [&](Rule &x) {
+				 x.d = 1.0 - x.w * accumulate(x.c.begin(), x.c.end(), 0.0,
+											  [](double c, auto &_) { return c + _.second; });
+				 rd *= x.d, ad *= 1.0 - x.w;
+				 for (int i = 0; i < CONS.size(); i++)
+					 beta[i] *= x.w * x.c[i] + x.d;
+			 });
+	sd = accumulate(beta.begin(), beta.end(), 0.0);
+	for_each(beta.begin(), beta.end(),
+			 [&](BeliefDegress &b) { b = (b - rd) / (sd - rd * ((int)CONS.size() - 1) - ad); });
+	return beta;
+}
+
+double output_result(BeliefDistribution &beta)
+{
+	double o = 0.0;
+	accumulate(beta.begin(), beta.end(), CONS.begin(),
+			   [&](auto c, BeliefDegress &b) {o+=*c*b;return ++c; });
+	return o;
+}
+
+double measure_antecedent(Antecedent &a, Antecedent &b)
+{
+	return inner_product(a.begin(), a.end(), b.begin(), numeric_limits<double>::max(),
+						 [](double c, double _) { return min(c, _); },
+						 [](Distribution &da, Distribution &db) { return match_individual(da, db); });
+}
+
+double measure_consequent(Consequent &a, Consequent &b)
+{
+	return match_individual(a, b);
+}
+
+double calculate_consistency(Rule &a, Rule &b)
+{
+	return exp(measure_antecedent(a.a, b.a) * (1.0 - measure_consequent(a.c, b.c)));
+}
+
+void calculate_consistency(vector<Rule> &r)
+{
 }
 
 int main()
 {
-	ios::sync_with_stdio(0),cout.setf(ios::fixed),cout.precision(3);
-
+	ios::sync_with_stdio(0), cout.setf(ios::fixed), cout.precision(3);
+	ATTR = {{-10.0, -8.0, -6.0, -4.0, -2.0, 0.0, 2.0},
+			{-0.02, -0.01, 0.0, 0.01, 0.02, 0.03, 0.04}};
+	CONS = {0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0};
+	DELTA = {1.0, 1.0};
 	ifstream I("../data/oil_testdata_2007.txt");
-	attribute A;vector<double> B;
-	A.push_back({-10.0, -8.0, -6.0, -4.0, -2.0, 0.0, 2.0});		//flowdiff
-	A.push_back({-0.02, -0.01, 0.0, 0.01, 0.02, 0.03, 0.04});	//pressurefiff
-	B={0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0};					//leaksize
+	ofstream O("result");
+	double fd, pd, ls;
+	double e, mse(0.0);
+	vector<Rule> r, train, test, tmp;
+	BeliefDistribution beta;
+	while (I >> fd >> pd >> ls)
+		r.push_back({{fd, pd}, ls});
+	random_shuffle(r.begin(), r.end());
+	train.assign(r.begin(), r.begin() + 1500);
+	test.assign(r.begin() + 1500, r.end());
+	for_each(test.begin(), test.end(), [&](Rule &x) {
+		activate_rule(x, train);
+		//tmp.clear(), tmp.reserve(train.size());
+		//copy_if(train.begin(), train.end(), back_inserter(tmp), [](Rule &y) { return y.w > EPS; });
+		//de(tmp.size());
+		//de(tmp[0].w);
+		beta = evidential_reasoning(train);
+		//for (int i = 0; i < CONS.size(); i++)
+		//	cout << "[" << CONS[i] << "]" << beta[i] << endl;
+		e = output_result(beta);
+		mse += (x.raw_c - e) * (x.raw_c - e);
+		O << x.raw_a[0] << " " << x.raw_a[1] << " " << x.raw_c << " " << e << endl;
+	});
+	cout << mse / test.size() << endl;
 }
--- code/view.py
+++ code/view.py
@@ -7,23 +7,27 @@ PressureDiff = np.arange(-0.02,0.04,0.01)
 
 x = []
 y = []
-z = []
+z1 = []
+z2 = []
 
-f = open("../data/oil_testdata_2007.txt","r")
+f = open("result","r")
 for line in f.readlines():
     line = str.split(line)
-    if len(line) == 3:
+    if len(line) == 4:
         y.append(float(line[0]))
         x.append(float(line[1]))
-        z.append(float(line[2]))
+        z1.append(float(line[2]))
+        z2.append(float(line[3]))
 
 x = np.array(x)
 y = np.array(y)
-z = np.array(z)
+z1 = np.array(z1)
+z2 = np.array(z2)
 
 fig = plt.figure()
 ax = Axes3D(fig)
-ax.scatter(x,y,z)
+ax.scatter(x,y,z1,c='b')
+ax.scatter(x,y,z2,c='r')
 ax.set_xlabel("PressureDiff")
 ax.set_ylabel("FlowDiff")
 ax.set_zlabel("LeakSize")

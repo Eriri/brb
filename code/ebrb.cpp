4cebff3031c3c71f72d54de27bf0b88c00ff80a9
eriri <741674999@qq.com>
2019-08-08 13:52:42 +0800

    190808

--- code/ebrb.cpp
+++ code/ebrb.cpp
@@ -1,6 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
+struct Rule
+
+    int
+    main()
 {
 }
--- z_main.py
+++ z_main.py
@@ -1,14 +1,17 @@
 import math
 import functools
 import operator
+import random
+
+attributes, results, positive = [], [], []
 
 
 class Rule:
-    def __init__(self, a, c):
+    def __init__(self, a, c, rw=1.0, uw=0.0):
         self.ant = trans(a)
         self.con = [0.0]*len(results)
         self.con[results.index(c)] = 1.0
-        self.rw, self.aw, self.uw = 0.0, 0.0, 0.0
+        self.rw, self.aw, self.uw = 0.0, rw, uw
 
 
 def trans(a):  # raw data transform into belief structure
@@ -32,6 +35,7 @@ def simi(a, b):  # calculate similarity of two belief distribution
     return max(0.0, 1.0-math.sqrt(sum([(c-d)*(c-d) for c, d in zip(a, b)])))
 
 
+'''
 def arw(rules):  # assign rule weight
     incon = [1.0*(len(rules)-1)] * len(rules)
     for i in range(len(rules)):
@@ -45,6 +49,7 @@ def arw(rules):  # assign rule weight
     sigincon = sum(incon)
     for i in range(len(rules)):
         rules[i].rw = 1.0 if sigincon == 0.0 else 1.0 - incon[i]/sigincon
+'''
 
 
 def caw(r, rules):  # calculate activate weight
@@ -53,6 +58,8 @@ def caw(r, rules):  # calculate activate weight
         rule.aw = functools.reduce(operator.mul, [simi(
             r.ant[i], rule.ant[i]) for i in range(len(attributes))], rule.rw)
         sw += rule.aw
+    if sw == 0.0:
+        return
     for rule in rules:
         rule.aw /= sw
 
@@ -63,22 +70,120 @@ def er(rules):  # evidential reasoning
     for k in range(len(results)):
         B[k] = functools.reduce(
             operator.mul, [r.aw*r.con[k]+1.0-r.aw for r in rules], 1.0)
-    S = sum(B[k])
+    S = sum(B)
+    if S == len(results)*D:
+        for k in range(len(results)):
+            B[k] = 1.0/len(results)
+        return B
     for k in range(len(results)):
         B[k] = (B[k]-D)/(S-len(results)*D)
     return B
 
 
-def adacost(rules):
-    pass
+def cs(rules, data):  # calculate score
+    error = 0.0
+    for r in data:
+        caw(r, rules)
+        B = er(rules)
+        error += r.uw * (B.index(max(B)) != r.con.index(max(r.con)))
+    return error
+
+
+def de(rules, data):
+    pwei, pe = [random.random() for i in range(len(rules))], float("inf")
+    bwei, be = pwei, float("inf")
+    cnt = 0
+    while cnt < 10:
+        nwei = pwei
+        for i in range(len(rules)):
+            if random.random() < 0.5:
+                nwei[i] = random.random()
+            if random.random() < 0.5:
+                nwei[i] = pwei[i]
+        for i in range(len(rules)):
+            rules[i].rw = nwei[i]
+        ne = cs(rules, data)
+        if ne < pe:
+            cnt = 0
+            pwei, pe = nwei, ne
+            if ne < be:
+                bwei, be = nwei, ne
+        else:
+            cnt += 1
+    for i in range(len(rules)):
+        rules[i].rw = bwei[i]
+
+
+def adacost(train, test, valid):
+    base, weight = [], []
+    for i in range(len(test)):
+        test[i].uw = 1.0 / len(test)
+    for i in range(10):
+        b = []
+        for i in range(len(test)):
+            b.append(train[random.randint(0, len(train)-1)])
+        de(b, test)
+        a = 0.5 * math.log(1.0/cs(b, test)-1) + math.log(len(results)-1)
+        base.append(b)
+        weight.append(a)
+        for r in test:
+            caw(r, b)
+            B = er(b)
+            tar, pre = r.con.index(max(r.con)), B.index(max(B))
+            r.uw *= math.exp((1-2*(tar == pre)+0.5*(tar in positive))*a)
+        s = sum([r.uw for r in test])
+        for r in test:
+            r.uw /= s
+    A = [[0] * len(results)] * len(results)
+    for r in valid:
+        T = [0.0] * len(results)
+        for b, w in zip(base, weight):
+            caw(r, b)
+            B = er(b)
+            for i in range(len(results)):
+                T[i] += w*B[i]
+        A[r.con.index(max(r.con))][T.index(max(T))] += 1
+    accuracy = sum([A[i][i] for i in range(len(results))])/sum(map(sum, A))
+    precision = A[5][5]/(sum([A[i][5] for i in range(len(results))])+1)
+    recall = A[5][5]/(sum([A[5][i] for i in range(len(results))])+1)
+    f1 = precision*recall/(precision+recall+1)
+    return accuracy, precision, recall, f1
+
+
+def kf(rules, k):  # 10 k fold
+    d = int(len(rules)/10)
+    valid = rules[d*k:d*(k+1)]
+    remain = rules[:d*k]+rules[d*(k+1):]
+    random.shuffle(remain)
+    return remain[int(len(remain)/4):], remain[:int(len(remain)/4)], valid
 
 
 if __name__ == "__main__":
     f = open('Testdata_214.txt')
     data = [x.split() for x in f.readlines()]
     data = [list(map(float, x)) for x in data]
-    global attributes, results
     attributes = [[min(x)+y*(max(x)-min(x))/4 for y in range(5)]
                   for x in zip(*data)][:-1]
     results = [0, 1, 2, 3, 4, 5, 6]
+    positive = [5]
     rules = [Rule(x[:-1], x[-1]) for x in data]
+    random.shuffle(rules)
+    cnt = 0
+    for r in rules:
+        caw(r, rules)
+        B = er(rules)
+        cnt += B.index(max(B)) == r.con.index(max(r.con))
+    print(cnt)
+
+    sum_acc, sum_pre, sum_rec, sum_f1 = 0, 0, 0, 0
+    for i in range(10):
+        train, test, valid = kf(rules, i)
+        accuracy, precision, recall, f1 = adacost(train, test, valid)
+        print("round-%d,accuracy(a):%f,precision(5):%f,recall(5):%f,f1:%f" %
+              (i+1, accuracy, precision, recall, f1))
+        sum_acc += 0.1*accuracy
+        sum_pre += 0.1*precision
+        sum_rec += 0.1*recall
+        sum_f1 += 0.1*f1
+    print("accuracy(a):%f,precision(5):%f,recall(5):%f,f1:%f" %
+          (sum_acc, sum_pre, sum_rec, sum_f1))

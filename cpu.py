1bfd644ab2c7571d8820fe3b8c86a63e04ec19db
Eriri <741674999@qq.com>
2019-09-02 21:06:31 +0800

    190902

--- cpu.py
+++ cpu.py
@@ -4,6 +4,11 @@ from sklearn.model_selection import StratifiedKFold
 from sklearn.decomposition import PCA
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from imblearn.metrics import geometric_mean_score
+from imblearn.under_sampling import RandomUnderSampler
+from imblearn.over_sampling import SMOTE
+from imblearn.under_sampling import ClusterCentroids
+from imblearn.combine import SMOTEENN
+from imblearn.combine import SMOTETomek
 from multiprocessing import Pool
 import tqdm
 
@@ -29,10 +34,6 @@ def EvidentialReasoing(ant, con, one, les, rw, a):
     if sw == 0.0:
         return np.random.randint(0, les.shape[0])
     wei = wei / sw
-    if len(wei[wei > 1e-6]) > 400:
-        print(len(wei[wei > 1e-6]))
-        print("ops")
-        exit(0)
     md = 1 - wei
     if np.min(md) == 0.0:
         return int(con[np.argmin(md)])
@@ -44,32 +45,40 @@ def EvidentialReasoing(ant, con, one, les, rw, a):
     return np.argmax(b)
 
 
-def GenerateBase(ant, con, one, les, cnt, bn, lo):
+def GenerateBase(ant, con, one, les,  cnt, bn, lo):
     skf = StratifiedKFold(n_splits=bn)
-    adsn = ADASYN(k=2, imb_threshold=0.6, ratio=1.0, verbose=False)
+    adsn = ADASYN(k=2, imb_threshold=0.6, ratio=0.5, verbose=False)
     pb = tqdm.tqdm(total=cnt*bn, position=lo)
     base = []
     for base_mask, test_mask in skf.split(ant, con):
         base_ant, base_con = ant[base_mask], con[base_mask]
         test_ant, test_con = ant[test_mask], con[test_mask]
-        base_ant, base_con = adsn.fit_transform(base_ant, base_con)
-        best, bw = 0.0, np.empty(base_ant.shape[0])
+        adsn.fit(base_ant, base_con)
+        good_ant = []
+        need_size = int(2.0 * np.sum(base_con == 0.0))
         for i in range(cnt):
-            rw = 1.0 - np.random.rand(base_ant.shape[0])
-            for ta, tc in zip(test_ant, test_con):
-                if les[EvidentialReasoing(base_ant, base_con, one, les, rw, ta)] != tc:
-                    tw = np.prod(np.exp(-np.abs((base_ant-ta)/one)), axis=1) * rw
-                    tw /= np.max(tw) * 4.0
-                    if tc == 1.0:
-                        rw[base_con == 0.0] /= 1.0 + tw[base_con == 0.0]
-                    if tc == 0.0:
-                        rw[base_con == 0.0] *= 1.0 + tw[base_con == 0.0]
-            pred_con = Evaluating([[base_ant, base_con, rw]], test_ant, one, les)
-            c = f1_score(1.0-test_con, 1.0-pred_con)
-            if c > best:
-                best, bw = c, rw
+            new_ant, new_con = adsn.oversample()
+            all_ant = np.concatenate([new_ant, base_ant[base_con == 1.0]])
+            all_con = np.concatenate([new_con, base_con[base_con == 1.0]])
+            new_size, rw = len(new_ant), np.ones((len(all_ant),))
+            for ta in test_ant[test_con == 1.0]:
+                if EvidentialReasoing(all_ant, all_con, one, les, 1.0, ta) != 1:
+                    tw = np.prod(np.exp(-np.abs((all_ant-ta)/one)), axis=1)
+                    rw[:new_size][tw[:new_size] > np.max(tw[:new_size])] = 0
+            good_ant.append(new_ant[rw[:new_size] == 1])
             pb.update()
-        base.append([base_ant, base_con, bw])
+        good_ant = np.concatenate(good_ant)
+        need_size = min(need_size, len(good_ant))
+        best = 0.0
+        for i in range(cnt):
+            np.random.shuffle(good_ant)
+            all_ant = np.concatenate([base_ant, good_ant[:need_size]])
+            all_con = np.concatenate([base_con, np.zeros((need_size,))])
+            fs = f1_score(1.0-test_con, 1.0-Evaluating(
+                [[all_ant, all_con, 1.0]], test_ant, one, les))
+            if best < fs:
+                best, best_ant, best_con = fs, all_ant, all_con
+        base.append([best_ant, best_con, 1.0])
     pb.close()
     return base
 
@@ -84,11 +93,24 @@ def Evaluating(base, test_ant, one, les, p=0):
     return np.array(test_con)
 
 
-def Ebrb(train_ant, train_con, test_ant, test_con, one, les):
+def Ebrb(ant, con, one, les, mask):
+    train_mask, test_mask = mask
+    train_ant, train_con = ant[train_mask], con[train_mask]
+    test_ant, test_con = ant[test_mask], con[test_mask]
     pred_con = []
     for ta in test_ant:
-        pred_con.append(EvidentialReasoing(train_ant, train_con, one, les, 1.0, ta))
+        pred_con.append(EvidentialReasoing(
+            train_ant, train_con, one, les, 1.0, ta))
     pred_con = np.array(pred_con)
+    return Result(test_con, pred_con)
+
+
+def Train(ant, con, one, les, mask, cnt, bn, lo):
+    train_mask, test_mask = mask
+    train_ant, train_con = ant[train_mask], con[train_mask]
+    test_ant, test_con = ant[test_mask], con[test_mask]
+    base = GenerateBase(train_ant, train_con, one, les, cnt, bn, lo)
+    pred_con = Evaluating(base, test_ant, one, les, 1)
     return Result(test_con, pred_con)
 
 
@@ -102,46 +124,40 @@ def Result(test_con, pred_con):
     return res
 
 
-def Train(ant, con, one, les, mask, cnt, bn, lo):
-    train_mask, test_mask = mask
-    train_ant, train_con = ant[train_mask], con[train_mask]
-    test_ant, test_con = ant[test_mask], con[test_mask]
-    base = GenerateBase(train_ant, train_con, one, les, cnt, bn, lo)
-    pred_con = Evaluating(base, test_ant, one, les, 1)
-    return Result(test_con, pred_con)
-
-
 def main():
     ant, con, one, les = ReadData("page-blocks.data")
     skf = StratifiedKFold(n_splits=10, shuffle=True)
     eva = ['accuracy', 'precision', 'recall', 'f1', 'g-mean']
 
-    # D = dict([(k, 0) for k in eva])
-    # p = Pool(10)
-    # cnt = 10  # 迭代次数
-    # bn = 5  # 基学习器个数
-    # res = [p.apply_async(Train, (ant, con, one, les, mask, cnt, bn, l,)) for l, mask in enumerate(skf.split(ant, con))]
-    # p.close(), p.join()
-    # res = [r.get() for r in res]
-    # for d in res:
-    #     print()
-    #     for k in eva:
-    #         D[k] += 0.1 * d[k]
-    # print(D)
-
-    for mask in skf.split(ant, con):
-        print(Train(ant, con, one, les, mask, 1, 3, 0))
-        break
-
+    D = dict([(k, 0) for k in eva])
+    p = Pool(10)
+    cnt = 10  # 迭代次数
+    bn = 5  # 基学习器个数
+    res = [p.apply_async(Train, (ant, con, one, les, mask, cnt, bn, l,))
+           for l, mask in enumerate(skf.split(ant, con))]
+    p.close(), p.join()
+    res = [r.get() for r in res]
+    for d in res:
+        print()
+        for k in eva:
+            D[k] += 0.1 * d[k]
+    print(D)
     # E = dict([(k, 0) for k in eva])
-    # p = Pool(10)
-    # res = [p.apply_async(Ebrb, (ant[t1], con[t1], ant[t2], con[t2], one, les)) for t1, t2 in skf.split(ant, con)]
-    # p.close(), p.join()
-    # res = [r.get() for r in res]
-    # for d in res:
-    #     for k in eva:
-    #         E[k] += 0.1 * d[k]
+    # for i in range(100):
+    #     p = Pool(10)
+    #     res = [p.apply_async(Ebrb, (ant, con, one, les, mask))
+    #            for mask in skf.split(ant, con)]
+    #     p.close(), p.join()
+    #     res = [r.get() for r in res]
+    #     for d in res:
+    #         for k in eva:
+    #             E[k] += 0.001 * d[k]
     # print(E)
+
+    # for mask in skf.split(ant, con):
+    #     print(Train(ant, con, one, les, mask, 10, 3, 0))
+    #     print(Ebrb(ant, con, one, les, mask))
+    #     break
 
 
 if __name__ == "__main__":
